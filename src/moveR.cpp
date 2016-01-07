67386d2b5e113b208d9c950b596a537b22f443e2
ChingHengWang <qoogood1234@gmail.com>
2016-01-07 23:41:05 +0800

    modify

--- src/moveR.cpp
+++ src/moveR.cpp
@@ -5,26 +5,32 @@
 #include <termios.h>
 #include <stdio.h>
 #include <sensor_msgs/Joy.h>
-
+#include <stdlib.h>
 struct point{
-  double x;
-  double y;
-  double z;
+  std_msgs::Float64 x;
+  std_msgs::Float64 y;
+  std_msgs::Float64 z;
 };
 
-
+struct arm_angle{
+  std_msgs::Float64 th_0;
+  std_msgs::Float64 th_1;
+  std_msgs::Float64 th_2;
+  std_msgs::Float64 th_3;
+};
  
 class MoveR
 {
 public:
   ros::Time current_time, last_time;
-  point Start_l,Target_l;
-  std_msgs::Float64 theta_l0,theta_l1,theta_l2,theta_l3;
+  point Target_left;
+  arm_angle left_arm_angle;
+  double length_link_up,length_link_down;
   void goalCallback(const geometry_msgs::Vector3& msg);
   MoveR();
   void IK_loop();
+  void IK(const point& p);
 
-private:
   ros::NodeHandle nh_;
   ros::Publisher axis_l0_pub,axis_l1_pub,axis_l2_pub,axis_l3_pub;
 protected:
@@ -32,19 +38,29 @@ protected:
 };
 
  void MoveR::goalCallback(const geometry_msgs::Vector3& vector){
-  Target_l.x=vector.x;
-  Target_l.y=vector.y;
-  Target_l.z=vector.z;
+  Target_left.x.data=1.0;
+  Target_left.y.data=vector.y;
+  Target_left.z.data=vector.z;
   return ; 
+}
+void MoveR::IK(const point& p)
+{
+  left_arm_angle.th_0.data=p.x.data;
+  left_arm_angle.th_1.data=p.y.data;
+  left_arm_angle.th_2.data=p.z.data;
+  left_arm_angle.th_3.data=0;
 }
   MoveR::MoveR()
 {
   current_time = ros::Time::now();
   last_time = ros::Time::now();
+  length_link_up=220,length_link_down=270;    
 
-  Target_l.x=0.0,Target_l.y=0.0,Target_l.z=0.0;
-  Start_l.x=0.0,Start_l.y=0.0,Start_l.z=0.0;
-  theta_l0.data=0.0,theta_l1.data=0.0,theta_l2.data=0.0,theta_l3.data=0.0;
+  left_arm_angle.th_0.data=0.0;
+  left_arm_angle.th_1.data=0.0;
+  left_arm_angle.th_2.data=0.0;
+  left_arm_angle.th_3.data=0.0;
+  Target_left.x.data=0.0,Target_left.y.data=0.0,Target_left.z.data=0.0;
 
   axis_l0_pub = nh_.advertise<std_msgs::Float64>("andbot/joint/L0/cmd/position", 1);
   axis_l1_pub = nh_.advertise<std_msgs::Float64>("andbot/joint/L1/cmd/position", 1);
@@ -58,11 +74,28 @@ protected:
 
 int main(int argc, char** argv)
 {
+
   ros::init(argc, argv, "moveR_left_arm");
   MoveR move_R;
+//  move_R.IK_loop();
+ printf("hello"); 
+  ros::Rate r(50.0);
 
-  move_R.IK_loop();
-  
+  while(move_R.nh_.ok())
+  {
+    ROS_INFO("x %f",move_R.Target_left.x.data);
+    ros::spinOnce;
+    move_R.IK(move_R.Target_left);
+    //current_time = ros::Time::now();
+    //th_l0.data=current_time.toSec();
+    move_R.axis_l0_pub.publish(move_R.Target_left.x);
+    move_R.axis_l1_pub.publish(move_R.left_arm_angle.th_1);
+    move_R.axis_l2_pub.publish(move_R.left_arm_angle.th_2);
+    move_R.axis_l3_pub.publish(move_R.left_arm_angle.th_3);
+
+    //last_time = ros::Time::now(); 
+    r.sleep();
+  }
   return(0);
 }
 
@@ -70,22 +103,19 @@ int main(int argc, char** argv)
 void MoveR::IK_loop()
 {
 
-   puts("IK_loop");
 
   ros::Rate r(50.0);
 
   while(nh_.ok())
   {
-
     ros::spinOnce; 
+    IK(Target_left);
     //current_time = ros::Time::now();
-    //theta_l0.data=current_time.toSec();
-    theta_l0.data=last_time.toSec();;
-    theta_l1.data=last_time.toSec();;
-    axis_l0_pub.publish(theta_l0);
-    axis_l1_pub.publish(theta_l1);
-    axis_l2_pub.publish(theta_l2);
-    axis_l3_pub.publish(theta_l3);
+    //th_l0.data=current_time.toSec();
+    axis_l0_pub.publish(Target_left.x);
+    axis_l1_pub.publish(left_arm_angle.th_1);
+    axis_l2_pub.publish(left_arm_angle.th_2);
+    axis_l3_pub.publish(left_arm_angle.th_3);
 
     //last_time = ros::Time::now(); 
     r.sleep();
